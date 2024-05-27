332743df6fee9cad0b06ca5e878f302dcac389bb
miguel-tds <migueltrrds@gmail.com>
2024-05-27 16:38:41 -0300

    erro no console.log

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(process.env.PORT || 3000, async () => {
-    console.log(`Alive at ${process.env.PORT}`);
+    console.log(`Alive at ${process.env.PORT || 3000}`);
     try {
         await client.connect();
         app.use("/users", UserRouter(client));
         console.log(`Connected to database`);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 });
