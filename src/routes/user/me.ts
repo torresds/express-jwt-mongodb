fc95decd23abbcbf322daad9757e22f56b886d1c
miguel-tds <migueltrrds@gmail.com>
2024-05-27 16:06:44 -0300

    criada rota para ver detalhes do usuario logado

--- src/routes/user/me.ts
+++ src/routes/user/me.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { UserRepo } from "../../repositories/interfaces/UserRepo";
+import { authenticatedRequest } from "../../middlewares/jwtAuth";
 
-export async function getMe(request: Request, response: Response, repo: UserRepo) {
-
+export async function getMe(request: authenticatedRequest, response: Response, repo: UserRepo) {
+    console.log(request.userId);
+    const userDetails = await repo.getById(request.userId!);
+    response.status(userDetails ? 200 : 404).json(userDetails);
 }
